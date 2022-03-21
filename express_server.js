e4036e1703796d7b49a7d1fcbd589c78c87a98c7
mariohamameh <mariohamameh97@gmail.com>
2022-03-21 22:52:25 +0000

    created short url

--- express_server.js
+++ express_server.js
@@ -19,7 +19,10 @@ app.get("/urls", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
-
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    res.render("urls_show", templateVars);
+});
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });

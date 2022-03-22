f635a799f98e31110e750d90a9e887f6df32faf5
mariohamameh <mariohamameh97@gmail.com>
2022-03-22 00:22:09 +0000

    tinyapp now can add urls to terminal

--- express_server.js
+++ express_server.js
@@ -3,11 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,6 +18,13 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
 });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);

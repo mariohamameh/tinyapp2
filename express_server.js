72d5f4ad6712c94b64c326a75e6d49e95475a7ba
mariohamameh <mariohamameh97@gmail.com>
2022-03-29 20:45:22 +0000

    add more code to delete route (if user is logged in or not)

--- express_server.js
+++ express_server.js
@@ -5,21 +5,18 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-//const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     name: "session",
     keys: ["user_id", "abc"],
-
     maxAge: 24 * 60 * 60 * 1000,
   })
 );
-//app.use(cookieParser());
-const bcrypt = require("bcryptjs");
-const password = "purple-monkey-dinosaur"; // found in the req.params object
+const bcrypt = require("bcryptjs");//for user hashed password
+const password = "purple-monkey-dinosaur"; 
 const hashedPassword = bcrypt.hashSync(password, 10);
-
+//root
 app.get("/", (req, res) => {
     if (req.session.user_id) {
         res.redirect("/urls")
@@ -33,54 +30,14 @@ app.get("/login", (req, res) => {
   };
   res.render("login", templateVars);
 });
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!email) {
-    return res.send("404");
-  }
-  const user = findUserByEmail(email, users);
-  if (!user) {
-    return res.send("404 email not found!");
-  }
-  if (!bcrypt.compareSync(password, findUserByEmail(email, users).password)) {
-    res.send("passwords dont match 403");
-  }
-  req.session.user_id = user.id;
-  return res.redirect("/urls");
-});
 
-app.post("/logout", (req, res) => {
-  req.session.user_id = null;
-  res.redirect("/urls");
-});
 app.get("/register", (req, res) => {
   const templateVars = {
     user: null,
   };
   return res.render("register", templateVars);
 });
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const user_id = generateRandomString();
-  if (!password || !email) {
-    res.send("404");
-  } else if (findUserByEmail(email, users)) {
-    res.send("404 email already exists!");
-  } else {
-    const user_id = generateRandomString();
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const user = {
-      email: email,
-      id: user_id,
-      password: hashedPassword,
-    };
-    users[user_id] = user;
-    req.session.user_id = user_id;
-    return res.redirect("/urls");
-  }
-});
+//check if user is logged in if not send him ot login page
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id) {
@@ -90,17 +47,7 @@ app.get("/urls", (req, res) => {
   const templateVars = { user: users[user_id], urls: userURLs };
   return res.render("urls_index", templateVars);
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL;
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id,
-  };
-  return res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
-});
-
+// creates new url
 app.get("/urls/new", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id) {
@@ -135,9 +82,23 @@ app.get("/u/:shortURL", (req, res) => {
     res.send("404 invalid url");
   }
 });
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id,
+  };
+  return res.redirect(`/urls/${shortURL}`);
+});
+//delets urls
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  return res.redirect("/urls");
+  if (req.session.user_id && req.session.user_id === urlDatabase[req.params.shortURL].userID) {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect('/urls');
+  } else {
+    res.send("401 not authorized to delete url");
+  }
 });
 app.post("/urls/:shortURL", (req, res) => {
   const userID = req.session.user_id;
@@ -153,9 +114,49 @@ app.post("/urls/:shortURL", (req, res) => {
     }
   }
 });
-
-
-
+//register user email password and user id if already registered send error
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user_id = generateRandomString();
+  if (!password || !email) {
+    res.send("404");
+  } else if (findUserByEmail(email, users)) {
+    res.send("404 email already exists!");
+  } else {
+    const user_id = generateRandomString();
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = {
+      email: email,
+      id: user_id,
+      password: hashedPassword,
+    };
+    users[user_id] = user;
+    req.session.user_id = user_id;
+    return res.redirect("/urls");
+  }
+});
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email) {
+    return res.send("404");
+  }
+  const user = findUserByEmail(email, users);
+  if (!user) {
+    return res.send("404 email not found!");
+  }
+  if (!bcrypt.compareSync(password, findUserByEmail(email, users).password)) {
+    res.send("passwords dont match 403");
+  }
+  req.session.user_id = user.id;
+  return res.redirect("/urls");
+});
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  res.redirect("/urls");
+});
+//start listening on port 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
